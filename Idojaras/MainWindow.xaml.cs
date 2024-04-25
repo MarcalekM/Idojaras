24dcfc394305697b2ac2d0c61c36ff93ecba0e91
Sufi <matemarcalek@gmail.com>
2024-04-25 19:59:49 +0200

    Finished

--- Idojaras/MainWindow.xaml.cs
+++ Idojaras/MainWindow.xaml.cs
@@ -16,27 +16,101 @@ namespace Idojaras
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Varos> varosok = new();
+        public List<string> adat { get; set; }
         public MainWindow()
         {
             InitializeComponent();
-            List<Varos> varosok = new();
+            P.Visibility = Visibility.Hidden;
+            H.Visibility = Visibility.Hidden;
+            SZ.Visibility = Visibility.Hidden;
+            PL.Visibility = Visibility.Hidden;
+            HL.Visibility = Visibility.Hidden;
+            SZL.Visibility = Visibility.Hidden;
+            telepules.Visibility = Visibility.Hidden;
+            Done.Visibility = Visibility.Hidden;
             varosok.Add(new("Miskolc", 15, 45, 39));
             varosok.Add(new("Budapest", 20, 42, 32));
             varosok.Add(new("Székesfehérvár", 12, 36, 37));
-            foreach (var v in varosok) ltbVarosok.Items.Add(v._nev);
-        }
+            Feltolt(varosok);
+        }   
 
-        private void button_Click(object sender, EventArgs e, List<Varos> varosok)
+        private void Varosok_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Button varos = (Button)sender;
-            string nev = varos.Content.ToString();
-            foreach (var v in varosok)
+            if (Varosok.SelectedItem != null)
             {
-                if (v._nev == nev)
+                ListBoxItem varos = (ListBoxItem)Varosok.SelectedItem;
+                bool kellAdat = true;
+                foreach (var v in varosok)
                 {
-                    Adatok.Text = v._paratartalom + "\n" + v._homerseklet + "\n" + v._szelsebesseg;
+                    if (v._nev == varos.Content.ToString())
+                    {
+                        Adatok.Text = $"Páratartalom: {v._paratartalom.ToString()} g/m3 \n Hőméréklet: {v._homerseklet.ToString()} °C\n Szélsebesség: {v._szelsebesseg.ToString()} km/h";
+                        kellAdat = false;
+                    }
+                }
+                if (kellAdat)
+                {
+                    P.Visibility = Visibility.Visible;
+                    H.Visibility = Visibility.Visible;
+                    SZ.Visibility = Visibility.Visible;
+                    PL.Visibility = Visibility.Visible;
+                    HL.Visibility = Visibility.Visible;
+                    SZL.Visibility = Visibility.Visible;
+                    telepules.Visibility = Visibility.Visible;
+                    Done.Visibility = Visibility.Visible;
+                    telepules.Text = varos.Content.ToString();
                 }
             }
+        }
+
+        private void Hozzaad(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem item = new();
+            item.Content = Telepules.Text;
+            Varosok.Items.Add(item);
+            Telepules.Text = string.Empty;
+        }
+
+        private void Feltolt(List<Varos> varosok)
+        {
+            Varosok.Items.Clear();
+            foreach (var v in varosok)
+            {
+                ListBoxItem varos = new();
+                varos.Content = v._nev;
+                Varosok.Items.Add(varos);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            varosok.Add(new(telepules.Text, int.Parse(H.Text), int.Parse(P.Text), int.Parse(SZ.Text)));
+            P.Visibility = Visibility.Hidden;
+            H.Visibility = Visibility.Hidden;
+            SZ.Visibility = Visibility.Hidden;
+            PL.Visibility = Visibility.Hidden;
+            HL.Visibility = Visibility.Hidden;
+            SZL.Visibility = Visibility.Hidden;
+            telepules.Visibility = Visibility.Hidden;
+            Done.Visibility = Visibility.Hidden;
+            Feltolt(varosok);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Adatok.Text != string.Empty) Adatok.Text = string.Empty;
+            ListBoxItem item = (ListBoxItem)Varosok.SelectedItem;
+            int i = -1;
+            foreach (var v in varosok)
+            {
+                if (v._nev == item.Content.ToString())
+                {
+                    i = varosok.IndexOf(v);
+                }
+            }
+            if(i  > -1 )varosok.RemoveAt(i);
+            Feltolt(varosok);
         }
     }
 }
