8b3fba36e8121472de7419664cee37a76e25ad0a
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2024-04-25 12:14:37 +0200

    Add project files.

---
+++ Idojaras/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Idojaras
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Varos> varosok = new();
+            varosok.Add(new("Miskolc", 15, 45, 39));
+            varosok.Add(new("Budapest", 20, 42, 32));
+            varosok.Add(new("Székesfehérvár", 12, 36, 37));
+            foreach (var v in varosok) ltbVarosok.Items.Add(v._nev);
+        }
+
+        private void button_Click(object sender, EventArgs e, List<Varos> varosok)
+        {
+            Button varos = (Button)sender;
+            string nev = varos.Content.ToString();
+            foreach (var v in varosok)
+            {
+                if (v._nev == nev)
+                {
+                    Adatok.Text = v._paratartalom + "\n" + v._homerseklet + "\n" + v._szelsebesseg;
+                }
+            }
+        }
+    }
+}
---
+++ Idojaras/Varos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idojaras
+{
+    internal class Varos
+    {
+        public string _nev;
+        public int _homerseklet;
+        public int _paratartalom;
+        public int _szelsebesseg;
+
+        public Varos(string nev, int homerselket, int paratartalom, int szelesebesseg)
+        {
+            _nev = nev;
+            _homerseklet = homerselket;
+            _paratartalom = paratartalom;
+            _szelsebesseg = szelesebesseg;
+        }
+    }
+}
